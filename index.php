62c1b713a3abda4c3f3cbe77a07ec0820f11d220
sitinurfarashikin <sitinurfarashikin@gmail.com>
2021-10-07 09:49:39 +0800

    beli baju

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Baju</title>
+    <style>
+        p{
+            font-size: 30px;
+            width: 500px;
+
+        }
+        body {
+            background-color: khaki;
+        }
+    </style>
+
+</head>
+<body>
+    <h1>SITI NUR FARASHIKIN BT ZULKIFLI</h1>
+
+    <?php 
+    $baju = 17;
+    $bawa = 50;
+    $hasil = $bawa;
+    $harga = 0;
+    $kira = 0;
+    while($hasil >= $baju) {
+        $hasil = $hasil - $baju;
+        $kira = $kira + 1;
+        $harga = $harga + $baju;
+    }
+?>
+
+<p>
+Saya membawa RM<?php echo $bawa; ?> . <br>
+    Baju sehelai berharga RM<?php echo $baju; ?> <br>
+    Saya dapat membeli <?php echo $kira ; ?>
+    helai baju dengan jumlah RM<?php echo $harga;
+    ?> berbaki RM <?php echo $hasil; ?>
+</p>
+</body>
+</html>
